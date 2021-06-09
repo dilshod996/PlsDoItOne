29224040477968ffc2f89a642e95708e0e2712ff
dilshod996 <dnurmamatoff@gmail.com>
2021-06-10 03:07:04 +0900

    firstedit

--- PleaseDoit/Assets/NumberWizard.cs
+++ PleaseDoit/Assets/NumberWizard.cs
@@ -11,11 +11,18 @@ public class NumberWizard : MonoBehaviour
     void Start()
     {
         Debug.Log("Welcome to my World!!");
+        Debug.Log("We have max in min points");
+        Debug.Log("Assigned max is: " + max);
+        Debug.Log("Assigned min is: " + min);
+        Debug.Log("Calculated number is: " + guess);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Debug.Log("Down key was pressed");
+        }
     }
 }
