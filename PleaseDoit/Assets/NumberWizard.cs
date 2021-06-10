dc3169a0cb272f66c20014946d016ac67de4b7f9
dilshod996 <dnurmamatoff@gmail.com>
2021-06-10 17:48:45 +0900

    Guessready

--- PleaseDoit/Assets/NumberWizard.cs
+++ PleaseDoit/Assets/NumberWizard.cs
@@ -10,11 +10,20 @@ public class NumberWizard : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        max = 1000;
+        min = 1;
+        guess = 500;
         Debug.Log("Welcome to my World!!");
         Debug.Log("We have max in min points");
         Debug.Log("Assigned max is: " + max);
         Debug.Log("Assigned min is: " + min);
         Debug.Log("Calculated number is: " + guess);
+        max++;
     }
 
     // Update is called once per frame
@@ -22,7 +31,23 @@ public class NumberWizard : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            Debug.Log("Down key was pressed");
+            min = guess;
+            GuessGame();
         }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            max = guess;
+            GuessGame();
+        }
+        else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("I am genius");
+            StartGame();
+        }
+    }
+    void GuessGame()
+    {
+        guess = (min + max) / 2;
+        Debug.Log("it is lower or higher than we expected:..." + guess);
     }
 }
