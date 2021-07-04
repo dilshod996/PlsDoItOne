82ac26abdf66893767d800a74721e150370569b7
dilshod996 <dnurmamatoff@gmail.com>
2021-07-04 09:17:38 +0900

    goooo
    initially

---
+++ Wordgame/Assets/State.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName ="State")]
+public class State : ScriptableObject
+{
+    [TextArea(10,14)] [SerializeField] string Storytext;
+    [SerializeField] State[] nextState;
+
+    public string GetStateStory()
+    {
+        return Storytext;
+    }
+    public State[] GetNextState()
+    {
+        return nextState;
+    }
+}
---
+++ Wordgame/Assets/TitleofGame.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TitleofGame : MonoBehaviour
+{
+    [SerializeField] Text textComponent;
+    [SerializeField] State StartingState;
+
+    State state;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = StartingState;
+        textComponent.text = state.GetStateStory();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
